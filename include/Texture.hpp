4c7ef977a573b5c6a461d8734e2c6f2acdd19ffb
Quentin Tennerel <quentin.tennerel@epitech.eu>
2024-04-29 23:59:32 +0200

    :sparkles: add texture

--- include/Texture.hpp
+++ include/Texture.hpp
@@ -7,6 +7,8 @@
 */
 
 #pragma once
+#include "GLSL/Color.hpp"
+#include "Material.hpp"
 
 class Texture {
 public:
@@ -15,7 +17,16 @@ public:
         SPECULAR,
         NORMAL,
         HEIGHT,
-
     };
-
+    Texture();
+    Texture(const Texture &texture);
+    Texture(const std::string &path, const TextureType type);
+    ~Texture() = default;
+    void setPath(const std::string &path);
+    void setType(const TextureType type);
+    const std::string &getPath() const;
+    const TextureType &getType() const;
+private:
+    std::string _path;
+    TextureType _type;
 };
